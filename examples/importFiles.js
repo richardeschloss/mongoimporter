9a94f8e41dced1d5919096930c948c983244f2ca
Richard Schloss <richard.e.schloss@gmail.com>
2019-03-19 11:32:05 -0700

    Code cleanup

--- examples/importFiles.js
+++ examples/importFiles.js
@@ -6,7 +6,7 @@ console.log('testConfig', testConfig)
 
 const mongoImporter = new MongoImporter(testConfig.mongoClient)
 mongoImporter.connect(testConfig.mongoClient.connectOptions)
-.then((resp) => mongoImporter.importDir(testConfig.importDir, testConfig.importDir_options))
+.then((resp) => mongoImporter.importFiles(testConfig.importFiles, testConfig.importFiles_options))
 .then(console.log)
 .catch(console.error)
 .finally(() => mongoImporter.disconnect() )
